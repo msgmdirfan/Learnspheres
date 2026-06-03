5cded32dd51539cafde42f84e7de1e72d951f795
msgmdirfan <msgmdirfan@gmail.com>
2026-06-03 11:41:18 +0530

    cofig edit

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-// const =  "http://localhost:3000"||process.env.REACT_APP_API_BASE_URL   ;
-const API_BASE_URL =process.env.REACT_APP_API_BASE_URL||"http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+
 export default API_BASE_URL;
